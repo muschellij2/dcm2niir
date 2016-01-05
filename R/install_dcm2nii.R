de2f7564e51a4531c440acc3719d8bb2a1d9880a
muschellij2 <muschellij2@gmail.com>
2016-01-05 13:26:26 -0500

    Added lib.loc to install_dcm2nii.

--- R/install_dcm2nii.R
+++ R/install_dcm2nii.R
@@ -2,19 +2,30 @@
 #' @description Install dcm2nii to dcm2niir for conversion tools 
 #' @return NULL
 #' @export
+#' @param lib.loc a character vector with path names of R libraries. 
+#' Passed to \code{\link{system.file}}  
 #' @examples 
 #' install_dcm2nii()
-install_dcm2nii = function(){
-  dcm2nii_files = system.file("dcm2nii", package= "dcm2niir")
+install_dcm2nii = function(lib.loc = NULL){
+  dcm2nii_files = system.file("dcm2nii", package = "dcm2niir",
+                              lib.loc = lib.loc)
   
-  if (!file.exists(dcm2nii_files)){
+  if (!file.exists(dcm2nii_files)) {
     url = "http://muschellij2.github.io/cttools/dcm2nii_files.zip"
-    urlfile <- file.path(system.file(package="dcm2niir"), "dcm2nii_files.zip")
-    download.file(url, urlfile, quiet=TRUE)
-    files = unzip(urlfile, exdir = system.file(package="dcm2niir"))
+    urlfile <- file.path(
+      system.file(package = "dcm2niir",
+                  lib.loc = lib.loc), 
+      "dcm2nii_files.zip")
+    download.file(url, urlfile, quiet = TRUE)
+    files = unzip(urlfile, 
+                  exdir = system.file(
+                    package = "dcm2niir", 
+                    lib.loc = lib.loc))
     for (ifile in files) system(sprintf("chmod +x %s", ifile))
     x = file.remove(urlfile)
   }
-  dcm2nii_files = system.file("dcm2nii", package= "dcm2niir")  
+  dcm2nii_files = system.file("dcm2nii", 
+                              package = "dcm2niir",
+                              lib.loc = lib.loc)  
   return(file.exists(dcm2nii_files))
 }
