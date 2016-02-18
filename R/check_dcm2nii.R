65507bf8f44bafd4931042fd54d5cbbe88329f86
muschellij2 <muschellij2@gmail.com>
2016-02-17 23:25:46 -0500

    check_dcm2nii

---
+++ R/check_dcm2nii.R
@@ -0,0 +1,35 @@
+#' @title Check Output from \code{dcm2nii}
+#' @description This function checks whether the output from \code{\link{dcm2nii}}
+#' runs correctly in that only one \code{nifti} file is returned if only one exists
+#'
+#' @param dcm2nii_output Output from \code{\link{dcm2nii}} command, must have
+#' \code{nii_after} element
+#'
+#' @return Character vector of unique nifti filenames
+#' @importFrom dplyr arrange group_by
+#' @export
+check_dcm2nii = function(dcm2nii_output){
+#   print(path)
+#   niis = dcm2nii(path)
+  niis = dcm2nii_output$nii_after
+  if (length(niis) > 1) {
+    bn = basename(niis)
+    bn = gsub("[.]gz$", "", bn)
+    bn = gsub("[.]nii$", "", bn)
+    ss = strsplit(bn, "_")
+    nc = nchar(bn)
+    l = min(sapply(ss, length))
+    ss = sapply(ss, function(x){
+      paste(x[seq(l)], collapse = "_")
+    })
+    df = data.frame(
+      name = niis,
+      stub = ss,
+      nc = nc,
+      stringsAsFactors = FALSE)
+    df = dplyr::arrange(df, stub, desc(nc))
+    df = head(dplyr::group_by(df, stub), 1)
+    niis = df$name
+  }
+  niis
+}
