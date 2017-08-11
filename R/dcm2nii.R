a4ee8ffce6e31982f5966b0ca741a1ec30f6dccf
muschellij2 <muschellij2@gmail.com>
2017-08-10 23:13:39 -0500

    Trying to fix example.

--- R/dcm2nii.R
+++ R/dcm2nii.R
@@ -16,7 +16,9 @@
 #' @export
 #' @examples 
 #' library(utils)
-#' dcm_file = "ftp://medical.nema.org/medical/Dicom/DataSets/WG30/MGH/MR/MouseBrainSiemens15T_20150410/Converted/DICOM/mghmousetoenhancedmr_T1w_pre.dcm"
+#' dcm_file = paste0("ftp://medical.nema.org/medical/Dicom/", 
+#' "DataSets/WG30/MGH/MR/MouseBrainSiemens15T_20150410/", 
+#' "Converted/DICOM/mghmousetoenhancedmr_T1w_pre.dcm")
 #' tdir = tempfile()
 #' dir.create(tdir)
 #' destfile = tempfile(fileext = ".dcm", tmpdir = tdir)
@@ -24,9 +26,10 @@
 #' method = ifelse(ci == "", "auto", "curl")
 #' dl = download.file(url = dcm_file, method = method, destfile = destfile)
 #' dl == 0
-#' file.exists(destfile)
+#' stopifnot(file.exists(destfile))
 #' dcm2niir::install_dcm2nii()
 #' res = dcm2niir::dcm2nii(basedir = tdir)
+#' stopifnot(res$result == 0)
 dcm2nii <- function(basedir = ".", 
                     copy_files = TRUE,
                     progdir = system.file(package = "dcm2niir"), 
