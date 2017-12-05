a1dda53b077845732c803490da44071b0a02012e
muschellij2 <muschellij2@gmail.com>
2017-12-04 21:55:17 -0500

    Fixing the installation.

--- R/install_dcm2nii.R
+++ R/install_dcm2nii.R
@@ -4,15 +4,17 @@
 #' @export
 #' @param lib.loc a character vector with path names of R libraries.
 #' Passed to \code{\link{system.file}}
+#' @param overwrite Force reinstallation if one already present
 #' @examples
 #' install_dcm2nii()
 #' @importFrom utils download.file unzip
 #' @importFrom httr stop_for_status GET write_disk progress
-install_dcm2nii = function(lib.loc = NULL){
+install_dcm2nii = function(lib.loc = NULL,
+                           overwrite = FALSE){
   dcm2nii_files = system.file("dcm2nii", package = "dcm2niir",
                               lib.loc = lib.loc)
-
-  if (!file.exists(dcm2nii_files)) {
+  
+  if (!file.exists(dcm2nii_files) || overwrite) {
     # url = "http://muschellij2.github.io/cttools/dcm2nii_files.zip"
     # url = paste0("https://github.com/muschellij2/cttools/",
     #              "raw/gh-pages/dcm2nii_files.zip")
@@ -24,13 +26,14 @@ install_dcm2nii = function(lib.loc = NULL){
       "dcm2nii_files.zip")    
     # download.file(url, urlfile)
     req = httr::GET(url, 
-                     httr::write_disk(path = urlfile),
-                     httr::progress())  
+                    httr::write_disk(path = urlfile, overwrite = overwrite),
+                    httr::progress())  
     httr::stop_for_status(req)
     files = unzip(urlfile,
                   exdir = system.file(
                     package = "dcm2niir",
-                    lib.loc = lib.loc))
+                    lib.loc = lib.loc),
+                  junkpaths = TRUE)
     for (ifile in files) system(sprintf("chmod +x %s", ifile))
     x = file.remove(urlfile)
     rm(x)
