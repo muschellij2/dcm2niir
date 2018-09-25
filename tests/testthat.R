dc6f4f6fa9fcc1041aab2b9fab274f99d218fb9f
muschellij2 <muschellij2@gmail.com>
2018-09-25 13:18:59 -0400

    More tests.

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(dcm2niir)
+
+test_check("dcm2niir")
---
+++ tests/testthat/test-install.R
@@ -0,0 +1,21 @@
+context("installing from source")
+
+test_that("install_dcm2nii", {
+  install_dir = tempfile()
+  dir.create(install_dir, showWarnings = FALSE)
+  install_dcm2nii(progdir = install_dir)
+  expect_true(install_dcm2nii(progdir = install_dir))
+})
+
+
+test_that("install_dcm2nii source", {
+  install_dir = tempfile()
+  dir.create(install_dir, showWarnings = FALSE)
+  cmake = Sys.which("cmake")
+  if (file.exists(cmake)) {
+    install_dcm2nii(progdir = install_dir, 
+                    from_source = TRUE,
+                    overwrite = TRUE)
+    expect_true(install_dcm2nii(progdir = install_dir))
+  }
+})
