d46defdc106396316be1416ec148cf575465e6e5
muschellij2 <muschellij2@gmail.com>
2019-08-29 15:55:51 -0400

    adding verbosity

--- R/dcm2nii.R
+++ R/dcm2nii.R
@@ -69,7 +69,7 @@ dcm2nii <- function(basedir = ".",
                       ifelse(ignore_derived, "-i y ", ""),
                       ifelse(merge_files, " -m y ", ""),
                       paste0(" -v ", as.numeric(verbose)),
-                      "-z y -f %p_%t_%s"),
+                      " -z y -f %p_%t_%s"),
                     ...){  
   dcm2niicmd = dcm2nii_bin(
     progdir = progdir,
