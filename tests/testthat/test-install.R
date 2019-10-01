613582e624366353069992645ccf9649d7bb96f8
muschellij2 <muschellij2@gmail.com>
2019-10-01 17:05:21 -0400

    added verbosity

--- tests/testthat/test-install.R
+++ tests/testthat/test-install.R
@@ -12,11 +12,14 @@ test_that("install_dcm2nii source", {
   install_dir = tempfile()
   dir.create(install_dir, showWarnings = FALSE)
   cmake = Sys.which("cmake")
+  make = Sys.which("make")
+  message("make is ", make)
   if (file.exists(cmake)) {
-    install_dcm2nii(progdir = install_dir, 
-                    from_source = TRUE,
-                    overwrite = TRUE,
-                    verbose = TRUE)
+    install_dcm2nii(
+      progdir = install_dir, 
+      from_source = TRUE,
+      overwrite = TRUE,
+      verbose = 2)
     expect_true(install_dcm2nii(progdir = install_dir))
   }
 })
